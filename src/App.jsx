fe9f5542478c8132fc4738e05cc94ca21a88c7a3
Yashavanthh <g.h.yashavanth@gmail.com>
2025-11-13 09:29:56 +0530

    1:alomost-complete

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+    </main>
+  )
+}
+
+export default App;
+
